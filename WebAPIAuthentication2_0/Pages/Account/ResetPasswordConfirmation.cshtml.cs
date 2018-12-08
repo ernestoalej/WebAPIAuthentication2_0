5a510bd182025a487445e6b68f5c7b00bf100f3a
FREDCOM-PC <FREDCOM-PC@FREDCOM-PC>
2018-12-07 23:10:14 -0400

    Agregar archivos de proyecto.

---
+++ WebAPIAuthentication2_0/Pages/Account/ResetPasswordConfirmation.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAPIAuthentication2_0.Pages.Account
+{
+    public class ResetPasswordConfirmationModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ WebAPIAuthentication2_0/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAPIAuthentication2_0.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
