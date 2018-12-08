942adde6793163048a21a9d4c2312a4c588dab83
FREDCOM-PC <FREDCOM-PC@FREDCOM-PC>
2018-12-08 00:20:28 -0400

    Configure Identity service

--- WebAPIAuthentication2_0/Startup.cs
+++ WebAPIAuthentication2_0/Startup.cs
@@ -33,6 +33,40 @@ namespace WebAPIAuthentication2_0
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+            // -----------Configure Identity services ----------------------
+            services.Configure<IdentityOptions>(options => 
+            {
+                // Password Settings
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredUniqueChars = 6;
+
+                //Lockout Setttings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+
+                //User Settings
+                options.User.RequireUniqueEmail = true;
+
+            });
+
+            services.ConfigureApplicationCookie(options => {
+                
+                //Cookie Settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+
+                options.LoginPath = "/Account/Login";    // If the LoginPath isn't set, ASP.NET Core defaults the path to /Account/Login.
+                options.AccessDeniedPath = "Account/AccessDenied";
+                options.SlidingExpiration = true;
+
+            });
+            // --------------------------------------------------------
+
             services.AddMvc()
                 .AddRazorPagesOptions(options =>
                 {
