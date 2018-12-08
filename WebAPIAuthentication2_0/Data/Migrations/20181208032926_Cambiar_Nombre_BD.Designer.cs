47c54d9a971b4dfee9dcb499b73aa21a5097996e
FREDCOM-PC <FREDCOM-PC@FREDCOM-PC>
2018-12-07 23:50:18 -0400

    Registrar Usuario

--- WebAPIAuthentication2_0/Data/Migrations/20181208032926_Cambiar_Nombre_BD.Designer.cs
+++ WebAPIAuthentication2_0/Data/Migrations/20181208032926_Cambiar_Nombre_BD.Designer.cs
@@ -11,9 +11,10 @@ using WebAPIAuthentication2_0.Data;
 namespace WebAPIAuthentication2_0.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181208032926_Cambiar_Nombre_BD")]
+    partial class Cambiar_Nombre_BD
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
