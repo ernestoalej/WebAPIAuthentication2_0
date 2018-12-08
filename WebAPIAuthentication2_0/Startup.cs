962e1e68642faaba89bec5c126eb40f9979a7e31
FREDCOM-PC <FREDCOM-PC@FREDCOM-PC>
2018-12-08 00:27:45 -0400

    Agregar routes en UseMvc

--- WebAPIAuthentication2_0/Startup.cs
+++ WebAPIAuthentication2_0/Startup.cs
@@ -97,7 +97,15 @@ namespace WebAPIAuthentication2_0
 
             app.UseAuthentication();
 
-            app.UseMvc();
+            app.UseMvc(routes=> {
+
+            routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}"                
+            );
+
+
+            });
         }
     }
 }
